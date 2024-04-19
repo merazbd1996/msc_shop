4a3472d7fe511ea48f024489dc611b27bced21a3
merazbd1996 <merazstudycare@gmail.com>
2024-04-20 00:27:12 +0600

    first commit

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class TImages {
+  static const String darkAppLogo = "assets/logos/whitlogo.png";
+  static const String lightAppLogo = "assets/logos/blacklogo.png";
+  //
+  static const String google = "assets/logos/blacklogo.png";
+  static const String facebook = "assets/logos/blacklogo.png";
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,13 @@
+class TText {
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTitle1 = "welcom to a world of limitless...";
+  static const String onBoardingSubTitle2 =
+      "for seamless Transactions, Choose your payment";
+  static const String onBoardingSubTitle3 = "from our doorstep to ....";
+
+  static const String homeAppbarTtile = "Good day for shopping";
+  static const String homeApppbarSubTitle = "Taimoor Sikander";
+}
