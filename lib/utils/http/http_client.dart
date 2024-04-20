f792c6ac4d185bab567622e2520aad025141e665
merazbd1996 <merazstudycare@gmail.com>
2024-04-20 21:25:12 +0600

    first commit

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:get/get.dart';
 
 class THttpHelper {
   static const String _baseUrl = 'http://your api';
+
   static Future<Map<String, dynamic>> get(String endpoint) async {
-    final reposne = await http.get(Uri.parse('$_baseUrl/$endpoint'));
+    final response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
+    return _handleResponse(response);
+  }
+
+  static Future<Map<String, dynamic>> post(
+      String endpoint, dynamic data) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/$endpoint'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data),
+    );
+    return _handleResponse(response);
+  }
+
+  static Future<Map<String, dynamic>> delete(
+      String endpoint, dynamic data) async {
+    final response = await http.delete(Uri.parse('$_baseUrl/$endpoint'));
+
+    return _handleResponse(response);
+  }
+
+  static Map<String, dynamic> _handleResponse(http.Response response) {
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data: ${response.statusCode}');
+    }
   }
 }
